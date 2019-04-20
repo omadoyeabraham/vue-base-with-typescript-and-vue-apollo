3e053e8a6d2051f714367db8f6e3178896b024a2
omadoyeabraham <omadoyeabraham@gmail.com>
2019-04-20 15:09:25 +0100

    [master] Added vue-apollo examples

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,3 +3,13 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import ApolloExample from "@/components/ApolloExample";
+
+export default {
+  components: {
+    ApolloExample
+  }
+};
+</script>
